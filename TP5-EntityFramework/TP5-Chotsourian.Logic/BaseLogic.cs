62f4c76374c60a78a38fbfc750a67f6b95b0ffe4
Marcos Chotsourian <chotsourian.marcos@gmail.com>
2021-05-03 20:25:05 -0300

    Menu movido a UI

---
+++ TP5-EntityFramework/TP5-Chotsourian.Logic/BaseLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP5_Chotsourian.Data;
+using TP5_Chotsourian.Entities;
+
+namespace TP5_Chotsourian.Logic
+{
+    public class BaseLogic
+    {
+        protected readonly NorthwindContext context;
+        public BaseLogic()
+        {
+            context = new NorthwindContext();
+        }
+    }
+}
